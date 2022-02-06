2b05acbc5c6fd8ae2f119c4858aa8954de8e5f1f
мансур зурабов <asl.zurabov@gmail.com>
2022-02-06 19:49:43 +0300

    categories in all templates

--- store/views.py
+++ store/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category,Product
 
+def categories(request):
+    return {
+        'categories':Category.objects.all()
+    }
+
 def all_products(request):
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
-    
+
