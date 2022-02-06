b632b9a55979486e0fb73ef7858565ab01028cd7
мансур зурабов <asl.zurabov@gmail.com>
2022-02-06 20:14:40 +0300

    detail for product

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from unicodedata import name
 from django.urls import path
-from .views import all_products
+from .views import all_products,product_detail
 app_name = 'store'
 urlpatterns = [
     path('',all_products,name='home'),
+    path('item/<slug:slug>/',product_detail,name='product_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category,Product
+from django.shortcuts import get_object_or_404
 
 def categories(request):
     return {
@@ -11,3 +12,7 @@ def all_products(request):
     products = Product.objects.all()
     return render(request,'home.html',{'products':products})
 
+
+def product_detail(request,slug):
+    product = get_object_or_404(Product,slug=slug,in_stock=True)
+    return render(request,'detail.html',{'product':product})
