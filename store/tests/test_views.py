15597311f06a5ed1c3907e9d7f070d6941bd1899
мансур зурабов <asl.zurabov@gmail.com>
2022-02-07 16:41:34 +0300

    store views testing

---
+++ store/tests/test_views.py
@@ -0,0 +1,10 @@
+from unittest import skip 
+from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category,Product
+
+# @skip('deponstarting skipping')
+# class TestSkip(TestCase):
+#     def test_skip_example(self):
+#         pass 
+
