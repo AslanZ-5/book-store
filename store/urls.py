f807a1b494d2102cc68dc783e41637ebb87bc1e2
мансур зурабов <asl.zurabov@gmail.com>
2022-02-08 19:12:24 +0300

    some urls has been changed

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from .views import all_products,product_detail,category_list
 app_name = 'store'
 urlpatterns = [
     path('',all_products,name='home'),
-    path('item/<slug:slug>/',product_detail,name='product_detail'),
-    path('search/<slug:category_slug>/',category_list,name='category_list')
+    path('<slug:slug>/',product_detail,name='product_detail'),
+    path('shop/<slug:category_slug>/',category_list,name='category_list')
 ]
