401facf0383614d92d449ac69fa34c641c5c716c
мансур зурабов <asl.zurabov@gmail.com>
2022-02-10 17:08:16 +0300

    sessions adding

--- basket/urls.py
+++ basket/urls.py
@@ -1,8 +1,9 @@
 from email.mime import base
 from unicodedata import name
 from django.urls import path
-from . views import basket_summary
+from . views import basket_summary,basket_add
 app_name = 'basket'
 urlpatterns = [
-    path('', basket_summary,name='basket_summary')
+    path('', basket_summary,name='basket_summary'),
+    path('add/', basket_add, name='basket_add'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from .basket import Basket
+from store.models import Product
 
 def basket_summary(request):
     return render(request,'basket/basket.html')
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basketqty = basket.__len__()
+        basket.add(product=product,qty=product_qty)
+        response = JsonResponse({'qty':basketqty})
+        return response
+
