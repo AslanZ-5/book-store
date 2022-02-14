a05ff981cabe8eb7905c5a0ebc06bdc304f94738
мансур зурабов <asl.zurabov@gmail.com>
2022-02-14 14:26:02 +0300

    login via built in authentication view

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .forms import UserLoginForm
 from .views import account_register, account_activate,dashboard
 
 app_name = 'account'
 urlpatterns = [
+    path('login/',auth_views.LoginView.as_view(template_name='account/registration/login.html',
+                                               form_class=UserLoginForm), name='login'),
     path('register/',account_register,name='register'),
     path('activate/<slug:uidb64>/<slug:token>/',account_activate,name='activate'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
 ]
