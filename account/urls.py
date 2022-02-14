8b6adf72a80631cd6e5355d1a8c3c7e407e066d7
мансур зурабов <asl.zurabov@gmail.com>
2022-02-14 17:33:12 +0300

    password reset part 3

--- account/urls.py
+++ account/urls.py
@@ -21,9 +21,10 @@ urlpatterns = [
                                                                  email_template_name='account/user/password_reset_email.html',
                                                                  form_class=PwdResetForm),name='pwdreset'),
     path('password_reset_confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(
-                                                                    template_name='account/user/password_reset_confirm.thml',
+                                                                    template_name='account/user/password_reset_confirm.html',
                                                                     success_url='password_reset_complete',
                                                                     form_class=PwdResetConfirmForm),name='password_reset_confirm'),
+    path('password_reset/password_reset_email_confirm',TemplateView.as_view(template_name='account/user/reset_status.html'), name='password_reset_done'),
     path('dashboard/', dashboard, name='dashboard'),
     path('profile/edit/', edit_details, name='edit_details'),
     path('profile/delete_user/', delete_user, name='delete_user'),
