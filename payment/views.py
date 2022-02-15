a246be5a0801c5425563fdc9881228b88653c0b4
мансур зурабов <asl.zurabov@gmail.com>
2022-02-15 14:45:16 +0300

    payment with stripe

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,21 @@
+from locale import currency
+import stripe
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from basket.basket import Basket
 
 @login_required
 def BasketView(request):
-    return render(request, 'payment/pay_home.html')
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace('.','')
+    total = int(total)
+    stripe.api_key = 'pk_test_51KTPH3Hhpp9pM3a6NdazrbU7xMfk6lYOMP1ug9mPmqiXowuRxn3W0Y7hHEbBKJDVUJh0LUgZqqkC61RHu1CshP9s00P2E0EUXX'
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency='gbp',
+        metadata={'userid':request.user.id}
+    )
+    return render(request, 'payment/pay_home.html',{'total':total})
