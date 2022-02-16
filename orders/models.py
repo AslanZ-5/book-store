657f7093ad6f768144e6c36ff344d520a23e79a8
мансур зурабов <asl.zurabov@gmail.com>
2022-02-16 13:48:58 +0300

    adding order when payment was executed

--- orders/models.py
+++ orders/models.py
@@ -11,7 +11,7 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order_user')
     full_name = models.CharField(max_length=50)
     address1 = models.CharField(max_length=250)
-    address1 = models.CharField(max_length=250)
+    address2 = models.CharField(max_length=250)
     city = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     post_code = models.CharField(max_length=20)
@@ -26,7 +26,8 @@ class Order(models.Model):
     
     def __str__(self):
         return str(self.created)
-    
+
+   
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
