02aa9aca12eebd354add30a3db0b71ad94f86192
мансур зурабов <asl.zurabov@gmail.com>
2022-02-26 10:39:29 +0300

    pytest fixtures

---
+++ tests/store/test_store.py
@@ -0,0 +1,10 @@
+
+import pytest
+# from django.test import TestCase
+
+# class Test_Class(TestCase):
+#     def test_hello_world(self):
+#         self.assertEqual('h','h')
+
+def test_hello_world():
+    assert "hell" == "hell"
