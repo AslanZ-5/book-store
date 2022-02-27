4305db92bb400518419871c8b87827bf9ef59d0f
мансур зурабов <asl.zurabov@gmail.com>
2022-02-27 12:00:03 +0300

    pytest problem solved

---
+++ tests/factories.py
@@ -0,0 +1,12 @@
+import django
+import factory
+from store.models import Category
+from faker import Faker 
+
+fake = Faker()
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model  = Category
+    name = 'django'
+    slug = 'django'
