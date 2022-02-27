71d9ab3061fcda935197e864628b67df13b38efe
мансур зурабов <asl.zurabov@gmail.com>
2022-02-27 13:09:10 +0300

    testing wiht pytest (category - absoluteurl and __str__)

--- store/tests/test_store_models.py
+++ store/tests/test_store_models.py
@@ -1,3 +1,14 @@
+from ast import arg
+from urllib import response
+import pytest
+from django.urls import reverse
 
 def test_category_str(product_category):
     assert product_category.__str__() == 'django'
+
+def test_category_reverse(client,product_category): # client allows to make ruquest 'GET'
+    category = product_category
+    url = reverse('store:category_list', args=[category.slug])
+    response = client.get(url)
+    assert response.status_code == 200
+    
